aa5a03d3897279c924099cbc427808403120311b
xixi_tech <xujuan@ceshiren.com>
2021-12-11 16:50:56 +0800

    appium 实战第二节课，封装PO app.py ,基类,page页面以及测试用例

---
+++ apptest/podemo/page/addresslist_page.py
@@ -0,0 +1,16 @@
+"""
+__author__ = 'hogwarts_xixi'
+"""
+# 通讯录页面
+from appium.webdriver.common.mobileby import MobileBy
+from appium.webdriver.webdriver import WebDriver
+
+from apptest.podemo.base.base_page import BasePage
+from apptest.podemo.page.addmember_page import AddMemberPage
+
+
+class AddressListPage(BasePage):
+    def click_addmember(self):
+        # click [添加成员]
+        self.find_and_click(MobileBy.XPATH, "//*[@text='添加成员']")
+        return AddMemberPage(self.driver)
