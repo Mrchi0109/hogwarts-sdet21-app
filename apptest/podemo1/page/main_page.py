a91c20a76be40ecd720dc0dc7b023fa4328f9989
xixi_tech <xujuan@ceshiren.com>
2021-12-12 15:03:47 +0800

    appium 实战第二节课，优化框架

---
+++ apptest/podemo1/page/main_page.py
@@ -0,0 +1,18 @@
+"""
+__author__ = 'hogwarts_xixi'
+"""
+# 首页
+from appium.webdriver.common.mobileby import MobileBy
+from appium.webdriver.webdriver import WebDriver
+
+from apptest.podemo1.base.base_page import BasePage
+from apptest.podemo1.page.addresslist_page import AddressListPage
+
+
+class MainPage(BasePage):
+    addresslist_element = (MobileBy.XPATH, "//*[@text='通讯录']")
+
+    def goto_addresslist(self):
+        # click [通讯录]
+        self.find_and_click(*self.addresslist_element)
+        return AddressListPage(self.driver)
